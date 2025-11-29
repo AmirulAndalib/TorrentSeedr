1e6589f9b0780030e57beda54dcbdf1cbf9c2dbe
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-11-29 21:08:11 +0545

    Add login command

---
+++ app/bot/views/login_view.py
@@ -0,0 +1,25 @@
+"""Views for login command."""
+
+from telethon import Button
+
+from app.bot.views import ViewResponse
+from app.utils.language import Translator
+
+
+def render_login_message(translator: Translator) -> ViewResponse:
+    message = translator.get("loginMessage")
+    buttons = [
+        [
+            Button.inline(
+                translator.get("loginWithEmailBtn"),
+                b"login_email",
+            )
+        ],
+        [
+            Button.inline(
+                translator.get("authorizeWithDeviceBtn"),
+                b"authorize_device",
+            )
+        ],
+    ]
+    return ViewResponse(message=message, buttons=buttons)
