02e3afd14a3c434b2a929a57dd7e264dadf0e9d7
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-12-06 08:12:42 +0545

    Add message handler

---
+++ app/bot/handlers/messages.py
@@ -0,0 +1,37 @@
+"""
+Message handlers for incoming text messages from reply keyboard.
+"""
+
+from telethon import events
+
+from app.bot.decorators import setup_handler
+from app.bot.handlers.commands.accounts import accounts_handler
+from app.bot.handlers.commands.active import active_torrents_handler
+from app.bot.handlers.commands.files import files_handler
+from app.bot.handlers.commands.info import info_handler
+from app.bot.handlers.commands.login import login_handler
+from app.database.models import User
+from app.utils.language import Translator
+
+
+@setup_handler(require_auth=False)
+async def text_message_handler(event: events.NewMessage.Event, user: User, translator: Translator, **kwargs):
+    """
+    Handles text messages from reply keyboard buttons.
+    TODO: Db call on User is happening twice in a single request, optimize it.
+    """
+    text = event.message.text
+
+    # Map button text to the corresponding handler function.
+    actions = {
+        translator.get("fileManagerBtn"): files_handler,
+        translator.get("activeTorrentsBtn"): active_torrents_handler,
+        translator.get("infoBtn"): info_handler,
+        translator.get("accountsBtn"): accounts_handler,
+        translator.get("loginBtn"): login_handler,
+    }
+
+    handler = actions.get(text)
+    if handler:
+        # Call the respective handler
+        await handler(event)
