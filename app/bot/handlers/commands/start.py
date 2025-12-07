c152a5b3660226a0fce5845d245ecbbea773675c
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-12-07 18:10:32 +0545

    Set bot commands

--- app/bot/handlers/commands/start.py
+++ app/bot/handlers/commands/start.py
@@ -3,20 +3,20 @@
 from telethon import events
 
 from app.bot.decorators import setup_handler
+from app.bot.utils.commands import set_user_commands
 from app.bot.views.start_view import render_start_message
-from app.database import get_session
 from app.database.models import User
-from app.database.repository import AccountRepository
 from app.utils.language import Translator
 
 
 @setup_handler()
 async def start_handler(event: events.NewMessage.Event, user: User, translator: Translator):
-    async with get_session() as session:
-        accounts = await AccountRepository(session).get_by_user_id(user.id)
+    has_accounts = bool(user.default_account_id)
+
+    await set_user_commands(event, translator, has_accounts)
 
     view = render_start_message(
-        has_accounts=len(accounts) > 0,
+        has_accounts=has_accounts,
         translator=translator,
     )
 
