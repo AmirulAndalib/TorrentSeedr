f2791dfeefca06d6735c9c60b46ac36ffa35d8ca
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-12-08 20:56:13 +0545

    Improve info view

--- app/bot/views/info_view.py
+++ app/bot/views/info_view.py
@@ -1,5 +1,7 @@
 """Views for account information."""
 
+from textwrap import dedent
+
 from seedrcc.models import AccountInfo
 
 from app.bot.views import ViewResponse
@@ -9,30 +11,27 @@ from app.utils.language import Translator
 
 def render_account_info(account_info: AccountInfo, translator: Translator) -> ViewResponse:
     username = account_info.username
-    email = account_info.email
-    user_id = account_info.user_id
+    bandwidth_used = account_info.bandwidth_used
+    invites_remaining = account_info.invites
+    invites_accepted = account_info.invites_accepted
     space_used = account_info.space_used
     space_max = account_info.space_max
-    bandwidth_used = account_info.bandwidth_used
-    premium = translator.get("yesLabel") if bool(account_info.premium) else translator.get("noLabel")
-    invites = account_info.invites
-    referral_link = f"https://www.seedr.cc/?r={user_id}"
+    referral_link = f"https://www.seedr.cc/?r={account_info.user_id}"
 
     progress = (space_used / space_max) * 100 if space_max > 0 else 0
     space_bar_visual = progress_bar(progress)
 
-    message = f"<b>{translator.get('accountInfoBtn')}</b>\n\n"
-    message += f"<b>{translator.get('usernameLabel')}</b> <code>{username}</code>\n"
-    message += f"<b>{translator.get('emailLabel')}</b> <code>{email}</code>\n"
-    message += f"<b>{translator.get('premiumLabel')}</b> {premium}\n\n"
+    message = dedent(f"""
+        <b>{translator.get("accountInfoBtn")}</b>
 
-    message += f"<b><u>{translator.get('storageLabel')}</u></b>\n"
-    message += f"<b>{translator.get('usedLabel')}</b> {format_size(space_used)} / {format_size(space_max)}\n"
-    message += f"{space_bar_visual}\n\n"
+        <b>{translator.get("usernameLabel")}</b> <code>{username}</code>
+        <b>{translator.get("bandwidthUsedTotalLabel")}</b> {format_size(bandwidth_used)}
+        <b>{translator.get("inviteLinkLabel")}</b> <code>{referral_link}</code>
+        <b>{translator.get("invitesAvailableLabel")}</b> {invites_remaining} / {account_info.max_invites}
+        <b>{translator.get("invitesAcceptedLabel")}</b> {invites_accepted}
 
-    message += f"<b>{translator.get('bandwidthUsedLabel')}</b> {format_size(bandwidth_used)}\n"
-    message += f"<b>{translator.get('invitesAvailableLabel')}</b> {invites}\n"
-    message += f"<b>{translator.get('referralLinkLabel')}</b> <code>{referral_link}</code>"
+        {format_size(space_used)} / {format_size(space_max)}
+        {space_bar_visual}
+    """)
 
-    return ViewResponse(message=message)
-
+    return ViewResponse(message=message.strip())
