f9af6e04e3f5c2d1c856ac8964f56a2338395d22
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-12-10 20:06:34 +0545

    Improve active downloads view

--- app/bot/views/active_downloads_view.py
+++ app/bot/views/active_downloads_view.py
@@ -1,42 +1,53 @@
 """Views for active downloads."""
+
 from textwrap import dedent
 
+from seedrcc.models import Torrent
 from telethon import Button
 
 from app.bot.views import ViewResponse
-from app.utils import format_size, progress_bar
+from app.utils import format_date, format_size, progress_bar
 from app.utils.language import Translator
 
 
-def render_download_status(download, translator: Translator) -> ViewResponse:
+def render_download_status(download: Torrent, translator: Translator) -> ViewResponse:
     """Renders the detailed progress message for a single download."""
-    name = download.name
+
     progress = int(download.progress)
-    size = download.size
-    downloaded = (progress / 100) * size if size else 0
+    title = download.name.strip()
+    downloaded_bytes = (progress / 100) * download.size if download.size else 0
+
     progress_visual = progress_bar(progress)
+    downloaded = format_size(downloaded_bytes)
+    size = format_size(download.size)
+    download_rate = format_size(download.download_rate)
+    last_update = format_date(download.last_update)
 
     message = dedent(f"""
-        <b>{translator.get('activeDownloadsBtn')}</b>
+        <b>{translator.get("activeDownloadsBtn")}</b>
+        <b>{"\n" + title if title else ""}</b>
+        <b>{translator.get("speedLabel")}</b> {download_rate}/s
+        <b>{translator.get("seedersLabel")}</b> {download.seeders}
+        <b>{translator.get("leechersLabel")}</b> {download.leechers}
+        <b>{translator.get("lastUpdateLabel")}</b> {last_update}
 
-        <b>{name}</b>
-           {progress_visual} {float(progress):.1f}%
-           {translator.get('sizeLabel')}: {format_size(downloaded)} / {format_size(size)}
-    """)
-    if download.download_rate:
-        speed = format_size(download.download_rate)
-        message += f"\n   {translator.get('speedLabel')}: {speed}/s"
+        <b>{translator.get("progressLabel")}</b> {downloaded} / {size} ({float(progress):.1f}%)
+        {progress_visual}
+
+        <b>{translator.get("pausedDownloadWarning") if download.stopped else ""}</b>
+    """).strip()
 
     buttons = [[Button.inline(translator.get("cancelBtn"), f"cancel_download_{download.id}".encode())]]
-    return ViewResponse(message=message.strip(), buttons=buttons)
+
+    return ViewResponse(message=message, buttons=buttons)
 
 
 def render_download_menu(active_downloads, translator: Translator) -> ViewResponse:
     """Render a menu of buttons for multiple active downloads."""
     message = dedent(f"""
-        <b>{translator.get('activeDownloadsBtn')}</b>
+        <b>{translator.get("activeDownloadsBtn")}</b>
 
-        {translator.get('selectDownload')}
+        {translator.get("selectDownload")}
     """)
     buttons = []
     for download in active_downloads:
