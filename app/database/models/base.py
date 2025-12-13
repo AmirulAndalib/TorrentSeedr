690b81f73f867ec7617bc0fadb0aa6a96c729cd0
Hemanta Pokharel <hemanta.pokharel@docsumo.com>
2025-12-13 19:39:20 +0545

    Set timezone to true

--- app/database/models/base.py
+++ app/database/models/base.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import DateTime, func
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
@@ -17,10 +17,12 @@ class TimestampMixin:
     """Mixin to add created_at and updated_at timestamps."""
 
     created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
         default=func.now(),
         nullable=False,
     )
     updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
         default=func.now(),
         onupdate=func.now(),
         nullable=False,
